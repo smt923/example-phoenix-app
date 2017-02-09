2100c5d243271b815e80e696210f1530f2da376f
smt <smtea923@gmail.com>
2017-02-09 01:30:12 +0000

    first commit

---
+++ web/router.ex
@@ -0,0 +1,46 @@
+defmodule Discuss.Router do
+  use Discuss.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Discuss do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", TopicController, :index
+  end
+
+  scope "/topics", Discuss do
+    pipe_through :browser
+
+    get "/", TopicController, :index
+    post "/", TopicController, :create
+    get "/new", TopicController, :new
+    put "/:id", TopicController, :update
+    delete "/:id", TopicController, :delete
+    get "/:id/edit", TopicController, :edit
+  end
+
+  scope "/auth", Discuss do
+    pipe_through :browser
+
+    get "/signout", AuthController, :signout
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", Discuss do
+  #   pipe_through :api
+  # end
+end
